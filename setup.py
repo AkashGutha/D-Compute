1e655bc9e99a3724b4d0ad82bce151abce239ef0
AkashGutha <akash.gutha@outlook.com>
2018-02-03 19:19:59 +0530

    ADD: initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='d-compute',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['d-compute'],
+    author='Akash Gutha',
+    author_email='akash.chowdary@outlook.com',
+    keywords=['dna', 'dna-computing', 'computing', 'programming', 'library'],
+    url='https://github.com/AkashGutha/D-Compute'
+)
